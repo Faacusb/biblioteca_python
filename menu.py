15a4b6797a6196a25e23bee2ad3d36c8cda856c9
Facu+ <Faacuusb@hotmail.com>
2025-06-09 11:04:54 -0300

    first commit

---
+++ menu.py
@@ -0,0 +1,34 @@
+def menu():
+    print('Bienvenido a nuestra biblioteca')
+    print('1. Agregar libro')
+    print('2. Mostrar libros')
+    print('3. Modificar libro')
+    print('4. Eliminar libro')
+    print('5. Salir')
+
+def abrir_menu():
+    while True:
+        menu()
+        opcion_menu = input('Elija una opcion: ')
+
+        if opcion_menu == '1':
+            agregar_libro()
+        
+        elif opcion_menu == '2':
+            mostrar_libros()
+
+        elif opcion_menu == '3':
+            modificar_libro()
+
+        elif opcion_menu  == '4':
+            eliminar_libro()
+
+        elif opcion_menu == '5':
+            print('Adios.')
+            break
+
+        else:
+            print('Opcion no valida, pruebe otra opcion.')   
+
+
+abrir_menu()
