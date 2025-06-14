64c41989a4bf8c180743fd745693d0f4677fa212
micaelacalesteballejo1997 <165739406+micaelacalesteballejo1997@users.noreply.github.com>
2025-06-14 15:08:23 -0300

    Create menu_PRINCIPAL.py

---
+++ menu_PRINCIPAL.py
@@ -0,0 +1,22 @@
+from libros import abrir_menu as menu_libros    #importe la funcion menu_libros 
+
+def menu_principal():
+    print("\n Menú Principal")
+    print("1. Libros")
+    print("2. Salir")
+
+def ejecutar_menu():
+    while True:
+       menu_principal()
+        opcion = input("Elegí una opción: ")
+
+        if opcion == '1':
+            menu_libros()  
+        elif opcion == '2':
+            print("¡nos vemos!")
+            break
+        else:
+            print(" Error. Probá otra vez.")
+
+if __name__ == "__main__": #esto es para que se ejecute directo 
+    iniciar_menu()
