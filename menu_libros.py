3e6b9e652763a133834a97f572a1619dbd1f9d44
Mariano Daniel Elean <elean.mariano@gmail.com>
2025-06-14 17:40:35 -0300

    Update menu_libros.py
    .json update

--- menu_libros.py
+++ menu_libros.py
@@ -1,35 +1,34 @@
-from libros import agregar_libro,mostrar_libros, modificar_libro,borrar_libro
-
+from libros import (
+    agregar_libro,
+    mostrar_libros,
+    modificar_libro,
+    borrar_libro,
+    cargar_libros
+)
 
 def abrir_menu():
+    cargar_libros()  # Cargar libros al iniciar
+
     while True:
-        
-        print('Bienvenido a nuestra biblioteca')
+        print('\nBienvenido a nuestra biblioteca')
         print('1. Agregar libro')
         print('2. Mostrar libros')
         print('3. Modificar libro')
         print('4. Borrar libro')
-        print('5. Volver al menu principal')
+        print('5. Salir')
 
-        opcion_menu = input('Elija una opcion: ')
+        opcion_menu = input('Elija una opción: ')
 
         if opcion_menu == '1':
             agregar_libro()
-        
         elif opcion_menu == '2':
             mostrar_libros()
-
         elif opcion_menu == '3':
             modificar_libro()
-
-        elif opcion_menu  == '4':
+        elif opcion_menu == '4':
             borrar_libro()
-
         elif opcion_menu == '5':
-            
+            print('Saliendo del programa.')
             break
-
         else:
-            print('Opcion no valida, pruebe otra opcion.')   
-
-
+            print('Opción no válida. Intente de nuevo.')
