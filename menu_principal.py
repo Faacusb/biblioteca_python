fc6612ae177b696551cf0b43cce31f5561aef297
Mariano Daniel Elean <elean.mariano@gmail.com>
2025-06-21 20:18:43 -0300

    Update menu_principal.py
    update with changes in module 3 and 4

--- menu_principal.py
+++ menu_principal.py
@@ -1,12 +1,17 @@
 from menu_libros import abrir_menu as menu_libros  #importe la funcion menu_libros 
 from menu_usuarios import menu_usuarios  #importe la funcion menu_usuarios
 from menu_prestamos import menu_prestamos
+from menu_prestamos import menu_prestamos  #importar prestamos
+from exportar_informe import exportar_informe #modulo para exportar a excel
+import requisitos  #Verifica e instala openpyxl si hace falta
+
 def menu_principal():
     print("\n Menú Principal")
     print("1. Libros")
     print("2. Usuarios")
     print("3. Prestamos")
-    print("4. Salir")
+    print("4. Exportar Informe")
+    print("5. Salir")
 
 def iniciar_menu():
     while True:
@@ -18,12 +23,14 @@ def iniciar_menu():
 
        elif opcion == '2':
             menu_usuarios()
-
-       elif opcion == "3":
-          menu_prestamos()
-
-            
+       
+       elif opcion == '3':
+            menu_prestamos()     
+       
        elif opcion == '4':
+            exportar_informe()     
+              
+       elif opcion == '5':
             print("¡nos vemos!")
             break
        else:
