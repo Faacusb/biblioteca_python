eb98e218e216fd33b18635d06b484acf96e0e15d
Mariano Daniel Elean <elean.mariano@gmail.com>
2025-06-21 22:18:28 -0300

    Update requisitos.py
    roll back

--- requisitos.py
+++ requisitos.py
@@ -1,13 +1,7 @@
-import subprocess
-import sys
-
-# Actualizar pip
-subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "pip"])
-
-# Luego, instalar openpyxl
 try:
     import openpyxl
 except ImportError:
+    import subprocess
+    import sys
     print("openpyxl no está instalado. Instalándolo ahora...")
     subprocess.check_call([sys.executable, "-m", "pip", "install", "openpyxl"])
-    import openpyxl  # Volvemos a intentar importar
